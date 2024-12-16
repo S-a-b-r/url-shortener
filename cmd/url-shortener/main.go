46163734f1321ac0f3ac42f05253176dde397e4f
S-a-b-r <po_fasty_grisha@mail.ru>
2024-12-17 00:21:54 +0400

    add pretty logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 
 	"github.com/S-a-b-r/url-shortener/internal/config"
+	mwLogger "github.com/S-a-b-r/url-shortener/internal/http-server/middleware"
+	"github.com/S-a-b-r/url-shortener/internal/lib/logger/handlers/slogpretty"
 	"github.com/S-a-b-r/url-shortener/internal/lib/logger/sl"
 	"github.com/S-a-b-r/url-shortener/internal/storage/sqlite"
 )
@@ -29,12 +31,22 @@ func main() {
 		os.Exit(1)
 	}
 
+	res, err := db.SaveURL("https://testUrl3rwer3243333", "https://te3st3")
+	if err != nil {
+		log.Error("error saving url", sl.Err(err))
+		os.Exit(1)
+	}
+	log.Debug("res", slog.Int64("resSaveUrl", res))
+
 	router := chi.NewRouter()
 
 	// middleware
 
 	router.Use(middleware.RequestID)
 	router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
 
 	// TODO: run server
 }
@@ -44,10 +56,22 @@ func setupLogger(env string) *slog.Logger {
 
 	switch env {
 	case envDev:
-		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		log = setupPrettySlog()
 	case envProd:
 		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	}
 
 	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
